f6c8e7c7b347ca929e53fa677cf9568af82ae5e6
Constantin Ionescu <ionescucosti@yahoo.com>
2025-02-25 09:16:14 +0200

    updated .gitignore

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn kolleno_challenge.wsgi --log-file -
+web: gunicorn --bind 0.0.0.0:8080 kolleno_challenge.wsgi:application --log-file -
--- kolleno_challenge/settings.py
+++ kolleno_challenge/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-)**4yrp54k_3qyxh_xp90lmbvdittb=@ehq94&7fh$+7$sogb=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.elasticbeanstalk.com', 'localhost']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Application definition
 
